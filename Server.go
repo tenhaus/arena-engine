5ca6a61db60f19fa186801f8b79644bb13cb1373
Chris Hayen <chayen@gmail.com>
2016-05-23 11:51:46 -0400

    Development key and output update

--- Server.go
+++ Server.go
@@ -11,6 +11,10 @@ func run() {
   timer := time.Tick(100 * time.Millisecond)
 
   for range timer {
-    fmt.Println("Tick")
+    // Check for new game requests
+    // Check the status of existing games
+    // Create or close games if needed
+
+    fmt.Println("0 Games 0 Players")
   }
 }
