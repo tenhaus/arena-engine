655e90119601e96e2efe3b0d32fc43bc448ce2dc
Chris Hayen <chayen@gmail.com>
2016-05-23 11:37:56 -0400

    Added game loop

--- Server.go
+++ Server.go
@@ -1,1 +1,18 @@
 // Ties it all together
+
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+  run()
+}
+
+func run() {
+  timer := time.Tick(100 * time.Millisecond)
+
+  for range timer {
+    fmt.Println("Tick")
+  }
+}
