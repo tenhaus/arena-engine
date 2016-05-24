83710adc8ddb5b7160d5cfa0e059b412c78a0a97
Chris Hayen <chayen@gmail.com>
2016-05-24 15:50:30 -0400

    Just comments on what I think I'm supposed to do

--- auth/Auth.go
+++ auth/Auth.go
@@ -2,5 +2,12 @@
 
 package auth
 
+// So I think we want to:
+//
+// Create a uuid for a user
+// Create a service account with the uuid
+// Somehow retrieve some token the user can use to authenticate
+// Send the token back
+
 func dosomething() {
 }
