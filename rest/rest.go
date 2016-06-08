2fd3d88e5f1f925ee2a393df56939d33130ea171
Chris Hayen <chayen@gmail.com>
2016-06-08 16:39:13 -0400

    Testing

--- rest/rest.go
+++ rest/rest.go
@@ -3,16 +3,33 @@ package rest
 import (
   "fmt"
   "net/http"
+  "encoding/json"
   "google.golang.org/appengine"
   "google.golang.org/appengine/log"
 )
 
+type SignupRequest struct {
+  Email string
+  Handle string
+  Password string
+}
+
 func init() {
-  http.HandleFunc("/", Signup)
+  http.HandleFunc("/signup", Signup)
 }
 
 func Signup(w http.ResponseWriter, r *http.Request) {
   context := appengine.NewContext(r)
+
+  decoder := json.NewDecoder(r.Body)
+  var signup SignupRequest
+
+  if err := decoder.Decode(&signup); err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    fmt.Fprint(w, err.Error())
+    return
+  }
+
   log.Infof(context, "This should go to the log")
-  fmt.Fprint(w, "Works")
+  fmt.Fprintf(w, "Signup |%v|", signup)
 }
