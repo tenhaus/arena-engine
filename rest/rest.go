7036a4cd1bce753f9ec2d0dd8ba46fc8f1a0b16f
Chris Hayen <chayen@gmail.com>
2016-06-08 17:00:10 -0400

    Removed the logging junk that didn't work

--- rest/rest.go
+++ rest/rest.go
@@ -4,8 +4,6 @@ import (
   "fmt"
   "net/http"
   "encoding/json"
-  "google.golang.org/appengine"
-  "google.golang.org/appengine/log"
 )
 
 type SignupRequest struct {
@@ -19,21 +17,18 @@ func init() {
 }
 
 func UseMe() {
-  
+
 }
 
 func Signup(w http.ResponseWriter, r *http.Request) {
-  context := appengine.NewContext(r)
-
   decoder := json.NewDecoder(r.Body)
   var signup SignupRequest
 
   if err := decoder.Decode(&signup); err != nil {
     w.WriteHeader(http.StatusInternalServerError)
-    fmt.Fprint(w, "Error: %s", err.Error())
+    fmt.Fprintf(w, "Error: %s", err.Error())
     return
   }
 
-  log.Infof(context, "This should go to the log")
   fmt.Fprintf(w, "Signup |%v|", signup)
 }
